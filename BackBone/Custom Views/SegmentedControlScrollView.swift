1b0d37e67f2264a16cd03e46bdfef17b17daef08
Andrea <Bellotto>
2017-11-24 11:20:21 +0100

    fix segmentedControlScrollView

--- BackBone/Custom Views/SegmentedControlScrollView.swift
+++ BackBone/Custom Views/SegmentedControlScrollView.swift
@@ -42,7 +42,7 @@ class SegmentedControlScrollView: UIView {
     
     public dynamic var selectedTextColor:UIColor?{
         get { guard let color = self.segmentedControlView.selectedTextColor else {
-                return Constants.SegmentedControl.TextColor.selectedPrimary
+            return Colors.Button.Text.selected
             }
             return color
         }
@@ -51,7 +51,7 @@ class SegmentedControlScrollView: UIView {
     
     public dynamic var deselectedTextColor:UIColor?{
         get { guard let color = self.segmentedControlView.deselectedTextColor else {
-            return Constants.SegmentedControl.TextColor.deselectedPrimary
+            return Colors.Button.Text.deselected
             }
             return color
         }
@@ -65,7 +65,7 @@ class SegmentedControlScrollView: UIView {
     
     public dynamic var selectedFont:UIFont?{
         get { guard let font = self.segmentedControlView.selectedFont else {
-            return Constants.Font.Regular.medium
+            return Font.SemiBold.small
             }
             return font
         }
@@ -74,7 +74,7 @@ class SegmentedControlScrollView: UIView {
     
     public dynamic var deselectedFont:UIFont?{
         get { guard let font = self.segmentedControlView.deselectedFont else {
-            return Constants.Font.Regular.medium
+            return Font.Regular.small
             }
             return font
         }
@@ -114,35 +114,28 @@ class SegmentedControlScrollView: UIView {
     }
     
     override func setup() {
-        let bundle = Bundle(for:type(of: self))
-        let nib = UINib(nibName: SegmentedControlScrollView.nameIdentifier, bundle: bundle)
-        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
-        view.frame = bounds
-        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        addSubview(view)
-        
+        super.setup()
         scrollView.isPagingEnabled = true
         scrollView.showsVerticalScrollIndicator = false
         scrollView.showsHorizontalScrollIndicator = false
         segmentedControlView.isHidden = (titles.count <= 1)
-
     }
     
     func setup(with viewControllers:[UIViewController], and titles:[String]){
-//        segmentedControlView.backgroundColor = kNavigationBackgroundColor
-//        
+        //        segmentedControlView.backgroundColor = kNavigationBackgroundColor
+        //
         self.views = [UIView]()
         self.heightAnchors = [NSLayoutConstraint]()
         self.widthAnchors = [NSLayoutConstraint]()
         self.viewControllers = viewControllers
         self.numElements = viewControllers.count
-        segmentedControlView.delegate = self
         stackView.translatesAutoresizingMaskIntoConstraints = false
         segmentedControlView.isHidden = (titles.count <= 1)
+        segmentedControlView.delegate = self
         scrollView.delegate = self
         self.layoutIfNeeded()
-
+        
+        
         segmentedControlView.setupTabs(with: titles, selectedTextColor: selectedTextColor!, deselectedTextColor: deselectedTextColor!, selectedFont: selectedFont!, deselectedFont: deselectedFont!)
         
         for el in stackView.subviews {
@@ -188,6 +181,10 @@ class SegmentedControlScrollView: UIView {
                     let index = self.scrollView.contentOffset.x / self.frame.size.width
                     self.segmentedControlView.selectTab(at: Int(index), animated:animated)
                     self.viewControllers[Int(index)].viewDidAppear(true)
+                    if let delegate = self.delegate {
+                        delegate.proto.didChangeViewTo(index: Int(index))
+                    }
+                    
                 }
             }
         }
@@ -197,37 +194,42 @@ class SegmentedControlScrollView: UIView {
             let index = self.scrollView.contentOffset.x / self.frame.size.width
             self.segmentedControlView.selectTab(at: Int(index), animated:animated)
             self.viewControllers[Int(index)].viewDidAppear(true)
+            if let delegate = self.delegate {
+                delegate.proto.didChangeViewTo(index: Int(index))
+            }
+            
         }
     }
 }
 
 
 extension SegmentedControlScrollView:UnderlinedSegmentedControlViewDelegate{
-    var buttonBackgroundColor: UIColor? {
-        return delegate?.viewController.navigationController?.navigationBar.backgroundColor
-    }
-
     func didSelectElement(at index: Int) {
         
-        UIView.animate(withDuration: 0.3, animations: { 
+        UIView.animate(withDuration: 0.3, animations: {
             self.scrollView.contentOffset.x = CGFloat(index) * self.frame.size.width
             
         }) { (completed) in
             if completed {
                 self.viewControllers[index].viewDidAppear(true)
+                if let delegate = self.delegate {
+                    delegate.proto.didChangeViewTo(index: Int(index))
+                }
             }
         }
     }
-    
 }
 
 
 extension SegmentedControlScrollView:UIScrollViewDelegate{
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-
+        
         let index = scrollView.contentOffset.x / self.frame.size.width
         segmentedControlView.selectTab(at: Int(index))
         viewControllers[Int(index)].viewDidAppear(true)
+        if let delegate = self.delegate {
+            delegate.proto.didChangeViewTo(index: Int(index))
+        }
     }
 }
 
